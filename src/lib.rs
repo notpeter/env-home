5805526a0993309ff3ad5016050f04abf0407bc4
Peter Tripp <petertripp@gmail.com>
2024-04-12 20:10:59 -0400

    Rename to env_home::env_home_dir. Switch to single monolithic test to avoid cargo test threads.

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ use std::env;
 use std::path::PathBuf;
 
 #[cfg(unix)]
-pub fn user_home_dir() -> Option<PathBuf> {
+pub fn env_home_dir() -> Option<PathBuf> {
     let home = env::var("HOME");
     match home {
         Ok(val) if !val.is_empty() => Some(PathBuf::from(val)),
@@ -11,7 +11,7 @@ pub fn user_home_dir() -> Option<PathBuf> {
 }
 
 #[cfg(windows)]
-pub fn home_dir() -> Option<PathBuf> {
+pub fn env_home_dir() -> Option<PathBuf> {
     let home = env::var("USERPROFILE");
     match home {
         Some(val) if !val.is_empty() => PathBuf::from(val),
@@ -20,34 +20,34 @@ pub fn home_dir() -> Option<PathBuf> {
 }
 
 #[cfg(all(not(windows), not(unix)))]
-pub fn home_dir() -> Option<PathBuf> {
+pub fn env_home_dir() -> Option<PathBuf> {
     None
 }
 
 #[cfg(test)]
 mod tests {
-    use super::user_home_dir;
+    use super::env_home_dir;
     use std::env;
     use std::path::PathBuf;
 
-    /* Possible future test cases:
+    /*
+    Note! Do not run these tests in parallel, as they modify the environment.
+    By default `cargo test` will run tests in parallel (multi-threaded) which
+    is unsafe and will cause intermittent panics. To run tests sequentially
+    use `cargo test -- --test-threads=1`.
+
+    More info:
+    - https://doc.rust-lang.org/std/env/fn.set_var.html
+    - https://github.com/rust-lang/rust/issues/27970
+
+    Possible future test cases:
     - Test non-windows/non-unix platforms (WASM, etc.)
     - Test non-utf8 paths (should return None)
     */
 
-    // Tests will fail if HOME/USERPROFILE unset on unix/windows
     #[test]
     #[cfg(any(unix, windows))]
-    fn empty_test() {
-        let old = std::env::var("HOME").unwrap();
-        std::env::set_var("HOME", "");
-        assert_eq!(user_home_dir(), None);
-        std::env::set_var("HOME", old);
-    }
-
-    #[test]
-    #[cfg(any(unix, windows))]
-    fn user_home_test() {
+    fn env_home_test() {
         let home_var = match env::consts::OS {
             "windows" => "USERPROFILE",
             _ => "HOME",
@@ -55,40 +55,21 @@ mod tests {
         let old = std::env::var(home_var).unwrap();
 
         // Sanity checks
-        assert_ne!(user_home_dir(), None, "HOME/USERPROFILE is unset");
-        assert_eq!(user_home_dir(), Some(PathBuf::from(old.clone())));
+        assert_ne!(env_home_dir(), None, "HOME/USERPROFILE is unset");
+        assert_eq!(env_home_dir(), Some(PathBuf::from(old.clone())));
 
         // Test when var unset.
-        std::env::remove_var(home_var);
-        assert_eq!(user_home_dir(), None);
+        env::remove_var(home_var);
+        assert_eq!(env_home_dir(), None);
 
         // Test when var set to empty string
-        std::env::set_var(home_var, "");
-        assert_eq!(user_home_dir(), None);
+        env::set_var(home_var, "");
+        assert_eq!(env_home_dir(), None);
 
-        std::env::set_var("USERPROFILE", old);
-    }
+        // Test a sensible unix value
+        std::env::set_var("HOME", "/tmp");
+        assert_eq!(env_home_dir(), Some(std::path::PathBuf::from("/tmp")));
 
-    #[test]
-    #[cfg(windows)]
-    /// Test an example windows user_home_dir
-    fn windows_home_test() {
-        let old = std::env::var("USERPROFILE").unwrap();
-        std::env::set_var("USERPROFILE", "C:\\Users\\Owner");
-        assert_eq!(super::home_dir(), None);
-        std::env::set_var("USERPROFILE", old);
-    }
-
-    #[test]
-    #[cfg(unix)]
-    /// Test an example unix user_home_dir
-    fn unix_home_test() {
-        let old = std::env::var("HOME").unwrap();
-        std::env::set_var("HOME", "/home/onthemoon");
-        assert_eq!(
-            user_home_dir(),
-            Some(std::path::PathBuf::from("/home/onthemoon"))
-        );
-        std::env::set_var("HOME", old);
+        env::set_var("USERPROFILE", old);
     }
 }
