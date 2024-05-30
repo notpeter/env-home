6869029ff53c78228937f22ccab86fa7797dcc66
Peter Tripp <petertripp@gmail.com>
2024-05-30 14:11:59 -0400

    Strip docstrings.

--- src/lib.rs
+++ src/lib.rs
@@ -1,43 +1,9 @@
-// Copyright 2024 Peter Tripp
-//! env_home is a general purpose crate for determining the current user
-//! home directory in a platform independant manner via enviornment variables.
-//!
-//! This crate is implemented in pure-rust and has no external dependencies.
-//!
-//! It is meant as a lightweight, drop-in replacement for `std::env::home_dir`
-//! provided by the Rust Standard Library which was
-//! [deprecated](https://doc.rust-lang.org/std/env/fn.home_dir.html#deprecation)
-//! in Rust 1.29.0 (Sept 2018).
-//!
-//! ## Usage
-//! ```rust
-//! use env_home::env_home_dir as home_dir;
-//! fn main() {
-//!     match home_dir() {
-//!         Some(path) => eprintln!("User home directory: {}", path.display()),
-//!         None => eprintln!("No home found. HOME/USERPROFILE not set or empty"),
-//!     }
-//! }
-//! ```
+// Copyright Peter Tripp
 
 use std::path::PathBuf;
 
 #[cfg(unix)]
 /// Returns the path of the current user’s home directory if known.
-///
-/// * On Unix, this function will check the `HOME` environment variable
-/// * On Windows, it will check the `USERPROFILE` environment variable
-/// * On other platforms, this function will always return `None`
-/// * If the environment variable is unset, return `None`
-/// * If the environment variable is set to an empty string, return `None`
-///
-/// Note: the behavior of this function differs from
-///   [`std::env::home_dir`](https://doc.rust-lang.org/std/env/fn.home_dir.html),
-///   [`home::home_dir`](https://docs.rs/home/latest/home/fn.home_dir.html), and
-///   [`dirs::home_dir`](https://docs.rs/dirs/latest/dirs/fn.home_dir.html).
-///
-/// This function returns `None` when the environment variable is set but empty.
-/// Those implementations return the empty string `""` instead.
 pub fn env_home_dir() -> Option<PathBuf> {
     let home = std::env::var("HOME");
     match home {
